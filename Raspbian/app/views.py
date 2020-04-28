4ac0225bdb1cf8f76b84239400cd16e5f9b21634
陈楷 <chenkai@admindeiMac.local>
2020-04-28 21:53:28 +0800

    historicaldata

--- Raspbian/app/views.py
+++ Raspbian/app/views.py
@@ -19,19 +19,6 @@ from picamera import PiCamera
 
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
-def thdata():
-    while True:
-        instance = dht11.DHT11(4)
-        result = instance.read()
-        if result.is_valid():
-            temp=result.temperature
-            hum =result.humidity
-            RPi.GPIO.cleanup(4)
-            now = time.strftime('%Y %m %d %H %M %S', time.localtime(time.time()))
-            thd = TH_FORM(timeval=now,temperature=temp,humidity=hum)
-            thd.save()
-        time.sleep(300)
-
 
 @login_required()
 def face(request):
