4c5f0c3f2f567a14e6bcf90f7b4e25ce44722e81
陈楷 <chenkai@admindeiMac.local>
2020-04-28 22:11:41 +0800

    historicaldata

--- Raspbian/app/tests.py
+++ Raspbian/app/tests.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 
 # Create your tests here.
 import time
+from app.models import TH_FORM
 
-now = time.strftime('%Y %m %d %H %M %S',time.localtime(time.time()))
-print(now)
+THD = TH_FORM.objects.all()
+print(type(THD))
+
+#now = time.strftime('%Y %m %d %H %M %S',time.localtime(time.time()))
+#print(now)
